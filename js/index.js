892c3ae99fd9b9f1fb40c99fbbf934f9de9e0073
Manuel Calvo Parra <manuelcalvoparra1@gmail.com>
2023-10-05 01:44:40 -0300

    Comentarios del codigo

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
+//Declaro constantes para las distintas secciones de la app.
 const container = document.querySelector('.container');
 const search = document.querySelector('.search button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.err');
 
+// Evento addlistener para la funcion "click"
+
 search.addEventListener('click', () =>{
+
+    //Consumir API y declarar variable del input de busqueda
 
     const APIKey = 'd68c47ff1a227b1929eeb165ea28bb8e'
     const city = document.querySelector('.search input').value;
@@ -12,9 +17,13 @@ search.addEventListener('click', () =>{
     if (city === '')
         return;
     
+    // Realizo un fetch para solicitar datos dentro de la api
+
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
         .then(response => response.json())
         .then(json => {
+
+    // Condicional para evaluar las distnitas respuestas de la app (404)
 
     if(json.cod === '404'){
         container.style.height = '500px';
@@ -28,12 +37,15 @@ search.addEventListener('click', () =>{
     error404.style.display = 'none';
     error404.classList.remove('fadeIn');
 
+    // Declaro constantes sobre los datos de diferentes resultados
+
     const image = document.querySelector('.weather-box img');
     const temperature = document.querySelector('.weather-box .temperature');
     const description = document.querySelector('.weather-box .description');
     const humidity = document.querySelector('.weather-details .humidity span');
     const wind = document.querySelector('.weather-details .wind span')
     
+    // Creo expresion switch para evaluar los diferentes casos del clima
 
         switch (json.weather[0].main){
             case "Clear":
@@ -59,6 +71,8 @@ search.addEventListener('click', () =>{
                 default:
                     image.src = "";
         }
+
+            // Mostrar resultados por pantalla
 
             temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
             description.innerHTML = `${json.weather[0].description}`;
